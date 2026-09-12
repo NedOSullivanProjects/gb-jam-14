aa2a879d7338a83336ade004e9061c1bb7f6abd6
Ned O'Sullivan <ned.osullivan.work@gmail.com>
2026-09-12 14:05:48 +0100

    updated player.gd
    updated movement restrictions to more closely emulated planned game feel.

--- player.gd
+++ player.gd
@@ -3,25 +3,31 @@ extends CharacterBody2D
 
 const SPEED = 10.0
 const JUMP_VELOCITY = -20.0#Temp values should be changed
-
+var lastXVelocity = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		velocity.x = lastXVelocity 
 	# Handle jump.
 	if Input.is_action_just_pressed("A Button") and is_on_floor(): #and not got double jump power
 		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("B Button"):
-		#handle attacking
-		pass
+		var direction := Input.get_axis("Left D-Pad", "Right D-Pad")
+		if direction:
+			lastXVelocity = direction * SPEED
+		else:
+			lastXVelocity = 0
+
 	
 	if Input.is_action_pressed("Up D-Pad") and Input.is_action_just_pressed("B Button"):
 		#perform second selected attack
 		pass
-	
+		
+	elif Input.is_action_just_pressed("B Button"):
+		#handle attacking
+		pass
+		
 	if Input.is_action_just_pressed("Select"):
 		#switch second weapon
 		pass
@@ -36,7 +42,8 @@ func _physics_process(delta: float) -> void:
 		if direction:
 			velocity.x = direction * SPEED
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = 0
 
-		move_and_slide()
+	move_and_slide()
 	#Is move_and_slide correct?
