b86d0f0e98024f10b53770fd54a70471aabc2186
Ned O'Sullivan <ned.osullivan.work@gmail.com>
2026-09-19 16:11:06 +0100

    added coin getting removed on pick up

--- Scenes/PickUp/coin.gd
+++ Scenes/PickUp/coin.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		#Player.add_gold(amount)
+		self.queue_free()
